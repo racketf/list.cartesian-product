6664e3980cd490300c40c7eadd3cd50478f21fda
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 17:17:25 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,19 +1,19 @@
 #lang racket
 
-; (product.sym sym sos): S X SL -> LL
+; (list.cartesian-product-sym sym sos): S X SL -> LL
 ; returns cartesian product of sym with sos
-(define product.sym
+(define list.cartesian-product-sym
   (lambda (sym sos)
     (if (null? sos)
         (list)
-        (cons (list sym (car sos)) (product.sym sym (cdr sos))))))
+        (cons (list sym (car sos)) (list.cartesian-product-sym sym (cdr sos))))))
 
-; (product sos1 sos2): SL X SL -> LL
+; (list.cartesian-product sos1 sos2): SL X SL -> LL
 ; returns cartesian product of sos1 with sos2
 ; : size(sos1)=0 -> ()
-; : else         -> (append (product.sym sos1[0] sos2) (product sos1[1..end] sos2))
-(define product
+; : else         -> (append (list.cartesian-product-sym sos1[0] sos2) (list.cartesian-product sos1[1..end] sos2))
+(define list.cartesian-product
   (lambda (sos1 sos2)
     (if (null? sos1)
         (list)
-        (append (product.sym (car sos1) sos2) (product (cdr sos1) sos2)))))
+        (append (list.cartesian-product-sym (car sos1) sos2) (list.cartesian-product (cdr sos1) sos2)))))
